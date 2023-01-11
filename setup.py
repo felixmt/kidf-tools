0a2e4dbff754e9a9cd53defb724f984ac5b5023f
Félix Motot <felix.motot@kisio.com>
2023-01-11 15:01:51 +0100

    update

--- setup.py
+++ setup.py
@@ -15,7 +15,6 @@ setup(
         long_description=LONG_DESCRIPTION,
         packages=find_packages(),
         install_requires=[
-            "os",
             "dotenv",
             "pandas",
             # "shapely",
