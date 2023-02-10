447bd54f65bef66381daa155ef32cfd634229bec
Félix Motot <felix.motot@kisio.com>
2023-02-10 11:30:42 +0100

    commit

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
         ], # add any additional packages that
         # needs to be installed along with your package. Eg: 'caer'
 
-        keywords=['python', 'first package'],
+        keywords=['python', 'fm tools package'],
         classifiers= [
             "Development Status :: 3 - Alpha",
             "Intended Audience :: Education",
