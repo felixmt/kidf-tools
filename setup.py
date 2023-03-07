ea31ad8051b50ce3916a882083528a572fd6da43
Félix Motot <felix.motot@kisio.com>
2023-03-07 11:24:28 +0100

    commit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 """
 from setuptools import setup, find_packages
 
-VERSION = '0.1.9'
+VERSION = '0.1.9.5'
 DESCRIPTION = 'Félix Motot python dependencies to deal with data'
 LONG_DESCRIPTION = 'Félix Motot python dependencies to deal with data'
 
